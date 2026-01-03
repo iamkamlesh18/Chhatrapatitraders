87e852cda0e0f8f1b65d5945643cc7a5970d08fd
Kamlesh Patil <65329245+iamkamlesh18@users.noreply.github.com>
2026-01-03 09:35:30 +0000

    "Initialize React + Vite project"

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>छत्रपती ट्रेडर्स – Dharangaon</h1>
+      <p>Construction Material & Hardware Supplier</p>
+    </div>
+  );
+}
+
+export default App;
